68025920774cc7b92d96999d306ffc7b9a339dca
Fabien Di Tore <fabien@ditore.ch>
2019-07-07 13:42:54 +0200

    ctd

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   "parserOptions": {
     "ecmaVersion": 8,
      "sourceType": "module",
+     "ecmaFeatures": {
+      "jsx": true
+    },
   },
   "parser": "babel-eslint",
   "env": {
