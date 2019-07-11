8bbd7cd6c8d8b9f2775072cf972117c5a6d1465e
Fabien Di Tore <fabien@ditore.ch>
2019-07-11 09:31:39 +0200

    finalized hoc

--- src/React/HOC/PublicComponent/hoc.js
+++ src/React/HOC/PublicComponent/hoc.js
@@ -4,7 +4,9 @@ Higher Order Component that redirect if user is  authenticated
 if there is a dispatch prop set to true, dispatch the connected action (dispatch_action). cf connect.js for  default action bound to gka auth if authenticated
 if there is a redirectTo Prop push the value to the history prop if not authenticated
 
-if there is a fallback prop defined
+if there is a fallback prop defined it is displayed for the props.timeout duration before redirect
+
+Dispatch is always immediate
 */
 
 import React, { Component } from 'react';
@@ -18,35 +20,42 @@ export default (ComposedComponent,_defaultProps)=> {
       this.timeout = null;
     }
     componentDidMount() {
-
-      this.props.check_session()
+      this.props.check_session().then(()=>{
+        this.logic();
+      })
     }
 
     componentDidUpdate(prevProps){
       if(!this.props.authenticated){
         this.setState({display_fallback:false})
       }
-
       if (!prevProps.authenticated && this.props.authenticated) {
-
-        if(typeof(this.props.fallback) == "undefined"){
-
-          if(typeof(this.props.redirectTo) != "undefined"){
-      //      this.props.history.push(this.props.redirectTo);
-          }
-
-
-        }else{
-          this.setState({display_fallback: true})
-          this.timeout = setTimeout(()=>{
-            this.props.history.push(this.props.redirectTo);
-            this.timeout = null;
-          },this.props.timeout);
-        }
-        if(this.props.dispatch === true){
-          this.props.dispatch_action();
-        }
+        this.logic();
       }
+    }
+
+    logic(){
+      if(typeof(this.props.history) == "undefined"){
+        throw new Error('history props is not defined, this HOC is meant to be used with react-router.')
+        return ;
+      }
+
+      if(typeof(this.props.fallback) == "undefined"){
+        if(typeof(this.props.redirectTo) != "undefined"){
+           this.props.history.push(this.props.redirectTo);
+        }
+      }else{
+        this.setState({display_fallback: true})
+        this.timeout = setTimeout(()=>{
+          this.props.history.push(this.props.redirectTo);
+          this.timeout = null;
+        },this.props.timeout);
+      }
+
+      if(this.props.dispatch === true){
+        this.props.dispatch_action();
+      }
+
     }
 
     componentWillUnmount(){
